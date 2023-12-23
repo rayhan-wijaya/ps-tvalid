324209a7ca9ae8457f7ef74a2ca024ea7ab0b9f0
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-12-23 17:37:14 +0700

    returning after writing to stdout

--- ps_tvalid.bash
+++ ps_tvalid.bash
@@ -21,16 +21,16 @@ fi
 
 validate_creds_ok='ok!'
 validate_creds() {
-    if [ "$prod_user" = '' ]; then echo '`prod_user` is not defined'; fi
-    if [ "$prod_pass" = '' ]; then echo '`prod_pass` is not defined'; fi
-    if [ "$prod_db"   = '' ]; then echo '`prod_db` is not defined';   fi
-    if [ "$prod_host" = '' ]; then echo '`prod_host` is not defined'; fi
-    if [ "$prod_port" = '' ]; then echo '`prod_port` is not defined'; fi
+    if [ "$prod_user" = '' ]; then echo '`prod_user` is not defined'; return; fi
+    if [ "$prod_pass" = '' ]; then echo '`prod_pass` is not defined'; return; fi
+    if [ "$prod_db"   = '' ]; then echo '`prod_db` is not defined';   return; fi
+    if [ "$prod_host" = '' ]; then echo '`prod_host` is not defined'; return; fi
+    if [ "$prod_port" = '' ]; then echo '`prod_port` is not defined'; return; fi
 
     # TODO: *efficiently* check the slave string as well
     #
-    if [ "$slaves"       = ''  ]; then echo '`slaves` is not defined'; fi
-    if [ "${#slaves[@]}" = '0' ]; then echo '`slaves` array is empty'; fi
+    if [ "$slaves"       = ''  ]; then echo '`slaves` is not defined'; return; fi
+    if [ "${#slaves[@]}" = '0' ]; then echo '`slaves` array is empty'; return; fi
 
     echo "$validate_creds_ok"
 }
