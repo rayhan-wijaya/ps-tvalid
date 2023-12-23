ab3dc5e0c3b5e3c4f3b002106d81ac9714daf284
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-12-23 20:33:29 +0700

    turned to exec_prod_db()

--- ps_tvalid.bash
+++ ps_tvalid.bash
@@ -42,12 +42,12 @@ then
     exit 1
 fi
 
-# $1: user
-# $2: pass
-# $3: db
-# $4: host
-# $5: port
-# $6: query
-exec_db() {
-    mariadb -u $1 -p"$2" -d $3 -h $4 -P $5 -e "$6"
+exec_prod_db() {
+    mariadb
+        -u $prod_user
+        -p"$prod_pass"
+        -d $prod_db
+        -h $prod_host
+        -P $prod_password
+        -e "$1"
 }
