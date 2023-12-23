69e7d951c78990705c185ea09f4777725a8ff356
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-12-23 17:32:22 +0700

    format if statements

--- ps_tvalid.bash
+++ ps_tvalid.bash
@@ -23,14 +23,14 @@ validate_creds_ok='ok!'
 validate_creds() {
     if [ "prod_user" = '' ]; then; return '`prod_user` is not defined'; fi
     if [ "prod_pass" = '' ]; then; return '`prod_pass` is not defined'; fi
-    if [ "prod_db" = '' ]; then; return '`prod_db` is not defined'; fi
+    if [ "prod_db"   = '' ]; then; return '`prod_db` is not defined';   fi
     if [ "prod_host" = '' ]; then; return '`prod_host` is not defined'; fi
     if [ "prod_port" = '' ]; then; return '`prod_port` is not defined'; fi
 
     # TODO: *efficiently* check the slave string as well
     #
-    if [ "slaves" = '' ]; then; return '`slaves` is not defined'; fi
-    if [ "${#slaves[@]}" = '0' ] then; return '`slaves` array is empty'; fi
+    if [ "slaves"        = ''  ]; then; return '`slaves` is not defined'; fi
+    if [ "${#slaves[@]}" = '0' ]; then; return '`slaves` array is empty'; fi
 
     return "$validate_creds_ok"
 }
